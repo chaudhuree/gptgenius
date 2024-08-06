fc8f2c8111366d7cc765d847f3fd833147fa9f48
chaudhuree <chaudhuree00@gmail.com>
2024-08-06 15:59:58 +0600

    basic clerk auth setup

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,8 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-
+import {
+  ClerkProvider
+} from '@clerk/nextjs'
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -12,7 +14,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <ClerkProvider>
       <body className={inter.className}>{children}</body>
+      </ClerkProvider>
     </html>
   );
 }
