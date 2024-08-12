d18989b0d71ee26b7ea349ee4e95e6c694b01790
chaudhuree <chaudhuree00@gmail.com>
2024-08-12 23:53:25 +0600

    in NewTour component react query is setup with loading state while fetching data based on isPending and conditional TourInfo show hide

--- components/NewTour.jsx
+++ components/NewTour.jsx
@@ -1,18 +1,37 @@
 'use client';
 
+import { useMutation } from '@tanstack/react-query';
+import { getExistingTour,generateTourResponse,createNewTour } from '@/utils/actions';
 import toast from 'react-hot-toast';
 import TourInfo from '@/components/TourInfo';
 
 const NewTour = () => {
+  const {
+    mutate,
+    isPending,
+    data: tour,
+  } = useMutation({
+    mutationFn: async (destination) => {
+      const newTour = await generateTourResponse(destination);
+      if (newTour) {
+        return newTour;
+      }
+      toast.error('No matching city found...');
+      return null;
+    },
+  });
   const handleSubmit = (e) => {
     e.preventDefault();
 
     const formData = new FormData(e.currentTarget);
     const destination = Object.fromEntries(formData.entries());
-    console.log('destination', destination);
+    // console.log('destination', destination);
+    mutate(destination);
     
   };
-
+  if(isPending){
+    return <span className='loading loading-lg'></span>;
+  }
   return (
     <>
       <form onSubmit={handleSubmit} className='max-w-2xl'>
@@ -38,7 +57,7 @@ const NewTour = () => {
         </div>
       </form>
       <div className='mt-16'>
-        <TourInfo />
+        <div className='mt-16'>{tour ? <TourInfo tour={tour} /> : null}</div>
       </div>
     </>
   );
--- utils/actions.js
+++ utils/actions.js
@@ -21,3 +21,15 @@ export const generateChatResponse = async (chatMessages) => {
     return null;
   }
 };
+
+export const getExistingTour = async ({ city, country }) => {
+  return null;
+};
+
+export const generateTourResponse = async ({ city, country }) => {
+  return null;
+};
+
+export const createNewTour = async (tour) => {
+  return null;
+};
