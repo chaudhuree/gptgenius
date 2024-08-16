53a15764a9bc9289550484bba9c7f3e4b04110ed
chaudhuree <chaudhuree00@gmail.com>
2024-08-17 00:58:30 +0600

    create new tour for the very first unique entry and also recheck either same data is already available in the database or not. if yes then do not go for openai api call . and fetch the data from mysql database

--- components/NewTour.jsx
+++ components/NewTour.jsx
@@ -1,22 +1,31 @@
-'use client';
+"use client";
 
-import { useMutation } from '@tanstack/react-query';
-import { getExistingTour,generateTourResponse,createNewTour } from '@/utils/actions';
-import toast from 'react-hot-toast';
-import TourInfo from '@/components/TourInfo';
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import {
+  getExistingTour,
+  generateTourResponse,
+  createNewTour,
+} from "@/utils/actions";
+import toast from "react-hot-toast";
+import TourInfo from "@/components/TourInfo";
 
 const NewTour = () => {
+  const queryClient = useQueryClient();
   const {
     mutate,
     isPending,
     data: tour,
   } = useMutation({
     mutationFn: async (destination) => {
+      const existingTour = await getExistingTour(destination); //check if the tour already exists
+      if (existingTour) return existingTour; //we will get this value in data:tour above
       const newTour = await generateTourResponse(destination);
       if (newTour) {
+        await createNewTour(newTour); //save the new tour
+        queryClient.invalidateQueries({ queryKey: ["tours"] }); //invalidate the cache
         return newTour;
       }
-      toast.error('No matching city found...');
+      toast.error("No matching city found...");
       return null;
     },
   });
@@ -27,37 +36,36 @@ const NewTour = () => {
     const destination = Object.fromEntries(formData.entries());
     // console.log('destination', destination);
     mutate(destination);
-    
   };
-  if(isPending){
-    return <span className='loading loading-lg'></span>;
+  if (isPending) {
+    return <span className="loading loading-lg"></span>;
   }
   return (
     <>
-      <form onSubmit={handleSubmit} className='max-w-2xl'>
-        <h2 className=' mb-4'>Select your dream destination</h2>
-        <div className='join w-full'>
+      <form onSubmit={handleSubmit} className="max-w-2xl">
+        <h2 className=" mb-4">Select your dream destination</h2>
+        <div className="join w-full">
           <input
-            type='text'
-            className='input input-bordered join-item w-full'
-            placeholder='city'
-            name='city'
+            type="text"
+            className="input input-bordered join-item w-full"
+            placeholder="city"
+            name="city"
             required
           />
           <input
-            type='text'
-            className='input input-bordered join-item w-full'
-            placeholder='country'
-            name='country'
+            type="text"
+            className="input input-bordered join-item w-full"
+            placeholder="country"
+            name="country"
             required
           />
-          <button className='btn btn-primary join-item' type='submit'>
+          <button className="btn btn-primary join-item" type="submit">
             generate tour
           </button>
         </div>
       </form>
-      <div className='mt-16'>
-        <div className='mt-16'>{tour ? <TourInfo tour={tour} /> : null}</div>
+      <div className="mt-16">
+        <div className="mt-16">{tour ? <TourInfo tour={tour} /> : null}</div>
       </div>
     </>
   );
